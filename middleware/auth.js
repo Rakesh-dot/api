795df030b5198a74514a3afc9720678bae8c5db0
Rakesh-dot <59675921+Rakesh-dot@users.noreply.github.com>
2021-02-05 10:45:05 +0545

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,46 @@
 //the function of the guard
+const { json } = require('express');
 const jwt=require('jsonwebtoken');
+const { findOne } = require('../models/proteinModel');
+const Register = require('../models/register_model');
 
 module.exports.verifyUser=function(req,res,next){
-    const token=req.headers.authorization.split(" ")[1];
-    const data=jwt.verify(token,'secretkey');
-    console.log(data.userId);
+    try{
+        const token=req.headers.authorization.split(" ")[1];
+        const data=jwt.verify(token,'secretkey');
+        Register.findOne({_id:data.userId})
+        .then(function(result){
+            req.userInfo=result;
+            next();
+        })
+        .catch(function(e){
+            res.status(401).json({error:e})
+        })
+    
+    }
+    catch(e){
+        res.status(401).json({error:e})
+    }
+}
+//guard for admin
+module.exports.verifyAdmin=function(req,res,next){
+    if(!req.userInfo){
+        return res.status(401).json({message:"Invalid User!!"});
+    }
+    else if(req.userInfo.userType!=='Admin'){
+        return req.status(401).json({message:"Unauthorized!!"})
+    }
+    next();
+}
+
+
+//guard for admin
+module.exports.verifyCustomer=function(req,res,next){
+    if(!req.userInfo){
+        return res.status(401).json({message:"Invalid User!!"});
+    }
+    else if(req.userInfo.userType!=='Customer'){
+        return req.status(401).json({message:"Unauthorized!!"})
+    }
     next();
 }
